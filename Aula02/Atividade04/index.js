dba39cbb0a7e5b169fd29897999e488d0ecfc621
russ1anyk <gustavogrelhus@gmail.com>
2025-02-28 15:47:29 -0300

    a

--- Aula02/Atividade04/index.js
+++ Aula02/Atividade04/index.js
@@ -3,6 +3,4 @@ const readline = require("readline-sync");
 const input = readline.question("Digite um número inteiro: ").trim();
 const num = Number(input);
 
-isNaN(num) || !Number.isInteger(num)
-  ? console.log("Entrada inválida! Por favor, digite um número inteiro.")
-  : console.log(num % 2 === 0 ? "O número é par" : "O número é ímpar");
+isNaN(num) || !Number.isInteger(num) ? console.log("Entrada inválida! Por favor, digite um número inteiro."): console.log(num % 2 === 0 ? "O número é par" : "O número é ímpar");   
